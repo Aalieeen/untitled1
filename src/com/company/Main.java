51b5c2bc630f15f2b3165277c46ac24677d1477a
1 <alexander.pavlov_a@s.ru>
2019-02-19 19:27:59 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int i =3;
+    int k =2;
+     i = count(i);
+     k = count(k);
+     System.out.println(i);
+     System.out.println(k);
+    }
+
+    private static int count(int x){
+x= x+1;
+return x;
+    }
+}
